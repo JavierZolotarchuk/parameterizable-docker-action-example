85eb6a6bf8876934efcc83504553fa18dd2fd294
JavierZolotarchuk <jzolotarchuk@10pines.com>
2020-04-19 14:29:04 -0300

    fix read variable in docker build command

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ echo "alpine_version $ALPINE_VERSION"
 
 cd dockerPosta
 echo "ejecutando docker build... charannnn"
-docker build -t javee --build-arg alpine_version=ALPINE_VERSION . && docker run javee
+docker build -t javee --build-arg alpine_version="$ALPINE_VERSION" . && docker run javee
 
 
