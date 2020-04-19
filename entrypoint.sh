370301ca1cb3d0b841cd29937bcfe91008423c3a
JavierZolotarchuk <jzolotarchuk@10pines.com>
2020-04-19 14:21:50 -0300

    alpine version from input

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,9 @@
 #!/bin/sh -l
 
-#echo "Hello $1"
-#time=$(date)
-#echo "::set-output name=time::$time"
-
-# shellcheck disable=SC2164
-#cd dockerPosta
-#docker build --build-arg alpine_version=3.9.5 .
-#echo "alojaaaaaaa"
-#docker --version
-#docker build .
-#echo "docker build OK"
-#docker build -t javee . && docker run -it javee
+echo "alpine_version $INPUT_ALPINE_VERSION"
 
 cd dockerPosta
 echo "ejecutando docker build... charannnn"
-docker build -t javee --build-arg alpine_version=3.9.5 . && docker run javee
+docker build -t javee --build-arg alpine_version="$INPUT_ALPINE_VERSION" . && docker run javee
 
 
