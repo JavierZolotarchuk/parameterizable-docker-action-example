28ecd04ab30c33f033489d8f246b78adfa752d0a
JavierZolotarchuk <jzolotarchuk@10pines.com>
2020-04-19 02:36:21 -0300

    prueba fantasma

---
+++ dockerPosta/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/sh -l
+
+cat /etc/alpine-release
+
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+#echo "Hello $1"
+#time=$(date)
+#echo "::set-output name=time::$time"
+
+# shellcheck disable=SC2164
+#cd dockerPosta
+#docker build --build-arg alpine_version=3.9.5 .
+echo "alojaaaaaaa"
+#docker --version
+#docker build .
+#echo "docker build OK"
+#docker build -t javee . && docker run -it javee
